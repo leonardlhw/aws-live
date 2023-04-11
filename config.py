2d3d26e7712ad903834919a1cfd5ae3d25b6cdc3
leonard <109149273+leonardlhw@users.noreply.github.com>
2023-04-11 16:46:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cbyegpcunxow.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leonardlohhanwei-employee"
+customregion = "us-east-1"
